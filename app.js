2139d03d561cfee768d6d2f808b559f70f520055
Judithmtzb <61176013+Judithmtzb@users.noreply.github.com>
2025-09-18 10:30:56 -0600

    Update app.js
    Se crea el codigo del amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,62 @@
-console.log ("Ejecutando Amigo Secreto");
+let amigos = []
+
+function agregarAmigo () {
+    let valorVacio = (document.getElementById('amigo').value);
+    if (valorVacio === ''){
+        alert ('Por favor, inserte un nombre');
+        return;
+    }  
+
+    amigos.push(document.getElementById('amigo').value);
+    console.log(amigos);
+
+    actualizarLista ();
+    limpiarCaja ();
+
+}
+
+function actualizarLista () {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML='';
+ 
+    //for (let i = 0; i < amigos.length; i++) {
+    amigos.forEach(amigos => {
+     lista.innerHTML += `<li>${amigos}</li>`;
+    
+    })
+    
+    
+}
+
+
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
+
+
+
+function sortearAmigo() {
+
+    if (amigos === ''){
+        alert ('No hay amigos disponibles para el sorteo');
+    } 
+
+    let indiceAleatorio = Math.floor(Math.random()*amigos.length);
+    console.log('Indice aleatorio generado', indiceAleatorio);
+
+    let amigoSorteado = amigos[indiceAleatorio];
+    console.log("Amigo sorteado:", amigoSorteado);
+
+    document.getElementById('resultado').innerHTML = `<li> El amigo Sorteado es ${amigoSorteado} </li>`;
+
+}
+
+function reiniciarJuego (){
+
+    amigos = [];
+    document.getElementById('listaAmigos').innerHTML='';
+    document.getElementById('resultado').innerHTML='';
+    alert ('Juego reiniciado. Puedes agregar nuevos amigos');
+    document.getElementById('btn-reiniciar').disabled = "true";
+}
+
